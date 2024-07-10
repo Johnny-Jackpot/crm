249afab2f8bcc7759b1d938fd819f39f169b7975
johnny <sanya.nazarenko.up036@gmail.com>
2024-07-11 02:09:39 +0300

    fixed property table name

--- database/migrations/2024_07_10_222429_create_property_table.php
+++ database/migrations/2024_07_10_222429_create_property_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('property', function (Blueprint $table) {
+        Schema::create('properties', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->nullable(false)->unique();
             $table->unsignedInteger('price');
