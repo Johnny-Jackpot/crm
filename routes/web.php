9408f328de55a578430f6dba52326888f0de3c08
johnny <sanya.nazarenko.up036@gmail.com>
2024-07-13 20:15:54 +0300

    added properties route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Resources\PropertyResource;
+use App\Models\Property;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/properties', function () {
+    return PropertyResource::collection(Property::paginate());
 });
