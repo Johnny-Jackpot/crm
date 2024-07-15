a45da44beb0ef08b5a0f27a4d09b8b126118ab42
johnny <sanya.nazarenko.up036@gmail.com>
2024-07-15 20:15:22 +0300

    select properties by params

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -5,14 +5,25 @@ namespace App\Http\Controllers;
 use App\Http\Requests\PropertyListRequest;
 use App\Http\Resources\PropertyResource;
 use App\Models\Property;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PropertyController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(PropertyListRequest $request)
+    public function index(PropertyListRequest $request): AnonymousResourceCollection
     {
-        return PropertyResource::collection(Property::paginate());
+        $qb = Property::selectList(
+            name: $request->get('name'),
+            priceFrom: $request->get('priceFrom'),
+            priceTo: $request->get('priceTo'),
+            bedrooms: $request->get('bedrooms'),
+            bathrooms: $request->get('bathrooms'),
+            storeys: $request->get('storeys'),
+            garages: $request->get('garages')
+        );
+
+        return PropertyResource::collection($qb->paginate());
     }
 }
