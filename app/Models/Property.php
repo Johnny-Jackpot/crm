bf5e10b1f77cdeeb5de6640c7af3cb6cbf091094
johnny <sanya.nazarenko.up036@gmail.com>
2024-07-15 19:30:54 +0300

    added method to search by parameters

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,47 @@ class Property extends Model
     use HasFactory;
 
     protected $table = 'properties';
+
+    public static function selectList(
+        ?string $name = null,
+        ?int    $priceFrom = null,
+        ?int    $priceTo = null,
+        ?int    $bedrooms = null,
+        ?int    $bathrooms = null,
+        ?int    $storeys = null,
+        ?int    $garages = null
+    ): Builder
+    {
+        $qb = self::query();
+
+        if ($name) {
+            $qb->whereColumn('name', 'LIKE', "%$name%");
+        }
+
+        if ($priceFrom !== null) {
+            $qb->where('price', '>=', $priceFrom);
+        }
+
+        if ($priceTo !== null) {
+            $qb->where('price', '<=', $priceTo);
+        }
+
+        if ($bedrooms !== null) {
+            $qb->where('bedrooms', '=', $bedrooms);
+        }
+
+        if ($bathrooms !== null) {
+            $qb->where('bedrooms', '=', $bedrooms);
+        }
+
+        if ($storeys !== null) {
+            $qb->where('storeys', '=', $storeys);
+        }
+
+        if ($garages !== null) {
+            $qb->where('garages', '=', $garages);
+        }
+
+        return $qb;
+    }
 }
