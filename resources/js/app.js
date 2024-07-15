9ea14c435c8163d48557f54b630c770996f6ba25
johnny <sanya.nazarenko.up036@gmail.com>
2024-07-15 23:57:16 +0300

    configured vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import Dashboard from './components/Dashboard.vue';
+
+createApp({})
+    .component('Dashboard', Dashboard)
+    .mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -27,4 +27,9 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
