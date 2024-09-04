db7f4ebe46a3b448c883bf566344764e699e1f9a
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-09-05 00:36:36 +0300

    added simple test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,13 +7,19 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+        $response->assertSee('Documentation');
+    }
+
+    public function test_the_application_contains_symfony(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+        $response->assertSee('Symfony');
     }
 }
