5300edb878d672b6a2673fd60227699783707005
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-09-06 18:42:00 +0300

    updated property test

--- tests/Feature/PropertyTest.php
+++ tests/Feature/PropertyTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Property;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -20,5 +21,12 @@ class PropertyTest extends TestCase
     {
         $response = $this->get(self::API_URL);
         $response->assertJsonCount(0, 'data');
+    }
+
+    public function test_api_property_table_non_empty(): void
+    {
+        Property::factory()->count(1)->create();
+        $response = $this->get(self::API_URL);
+        $response->assertJsonCount(1, 'data');
     }
 }
