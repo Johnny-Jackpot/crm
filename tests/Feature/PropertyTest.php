5cdd1bf0c786e4803a545d2685bf112f53f122e3
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-09-06 17:52:05 +0300

    refactored property tests

--- tests/Feature/PropertyTest.php
+++ tests/Feature/PropertyTest.php
@@ -8,15 +8,17 @@ use Tests\TestCase;
 
 class PropertyTest extends TestCase
 {
+    private const API_URL = '/api/property';
+
     public function test_api_property_reachable(): void
     {
-        $response = $this->get('/api/property');
+        $response = $this->get(self::API_URL);
         $response->assertStatus(200);
     }
 
     public function test_api_property_table_empty(): void
     {
-        $response = $this->get('/api/property');
+        $response = $this->get(self::API_URL);
         $response->assertJsonCount(0, 'data');
     }
 }
