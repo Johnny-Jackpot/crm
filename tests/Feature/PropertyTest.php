c0e2a5f4936a173deb2645a6977897be1df1c050
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-09-10 01:01:26 +0300

    added sanctum to tests

--- tests/Feature/PropertyTest.php
+++ tests/Feature/PropertyTest.php
@@ -3,8 +3,10 @@
 namespace Tests\Feature;
 
 use App\Models\Property;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class PropertyTest extends TestCase
@@ -15,18 +17,27 @@ class PropertyTest extends TestCase
 
     public function test_api_property_reachable(): void
     {
+        Sanctum::actingAs(
+            User::factory()->create(),
+        );
         $response = $this->getJson(self::API_URL);
         $response->assertStatus(200);
     }
 
     public function test_api_property_table_empty(): void
     {
+        Sanctum::actingAs(
+            User::factory()->create(),
+        );
         $response = $this->getJson(self::API_URL);
         $response->assertJsonCount(0, 'data');
     }
 
     public function test_api_property_table_non_empty(): void
     {
+        Sanctum::actingAs(
+            User::factory()->create(),
+        );
         Property::factory()->count(1)->create();
         $response = $this->getJson(self::API_URL);
         $response->assertJsonCount(1, 'data');
@@ -34,6 +45,9 @@ class PropertyTest extends TestCase
 
     public function test_api_property_route_doesnt_contain_11th_record(): void
     {
+        Sanctum::actingAs(
+            User::factory()->create(),
+        );
         $properties = Property::factory(11)->create();
         $lastProperty = $properties->last();
         $this->getJson(self::API_URL)
