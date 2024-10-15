e2df99cebbabcfb03424b83f8b1b288e87feb6c4
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-10-15 15:41:20 +0300

    fixed login method

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\UserHasRoles;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
+Route::post('login', [LoginController::class, 'authenticate'])->name('login');
 Route::apiResource('properties', PropertyController::class);
 Route::apiResource('users', UserController::class)
     ->only(['index', 'store'])
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::post('login', [LoginController::class, 'authenticate'])->name('login');
